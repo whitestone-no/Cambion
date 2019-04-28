979b00f92ca60c41256685d4c9b317e45d1f8d5a
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2019-04-28 22:10:18 +0200

    Added functionality to ISerializer, and implementation in JsonNet Serializer

--- src/Common/Interfaces/ISerializer.cs
+++ src/Common/Interfaces/ISerializer.cs
@@ -1,6 +1,10 @@
-﻿namespace Whitestone.Cambion.Common.Interfaces
+﻿using Whitestone.Cambion.Common.Types;
+
+namespace Whitestone.Cambion.Common.Interfaces
 {
     public interface ISerializer
     {
+        byte[] Serialize(MessageWrapper messageBytes);
+        MessageWrapper Deserialize(byte[] serialized);
     }
 }
--- src/Serializers/JsonNet/JsonNetSerializer.cs
+++ src/Serializers/JsonNet/JsonNetSerializer.cs
@@ -1,8 +1,26 @@
-﻿using Whitestone.Cambion.Common.Interfaces;
+﻿using Newtonsoft.Json;
+using System.Text;
+using Whitestone.Cambion.Common.Interfaces;
+using Whitestone.Cambion.Common.Types;
 
 namespace Whitestone.Cambion.Serializers.JsonNet
 {
     public class JsonNetSerializer : ISerializer
     {
+        public MessageWrapper Deserialize(byte[] messageBytes)
+        {
+            string json = Encoding.ASCII.GetString(messageBytes);
+            MessageWrapper wrapper = JsonConvert.DeserializeObject<MessageWrapper>(json, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+
+            return wrapper;
+        }
+
+        public byte[] Serialize(MessageWrapper messageBytes)
+        {
+            string json = JsonConvert.SerializeObject(messageBytes, Formatting.None, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+            byte[] rawBytes = Encoding.ASCII.GetBytes(json);
+
+            return rawBytes;
+        }
     }
 }
