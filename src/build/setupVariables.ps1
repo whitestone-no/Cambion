ce084a692e814f112e6abc3754c079e0cf7f96e0
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2019-06-15 23:14:57 +0200

    Moved GitVersion to "build" folder. Renamed gitversion.ps1 to setupVariables.ps1. Readded GitVersion.yml for better PreReleaseLabel handling.

--- src/build/setupVariables.ps1
+++ src/build/setupVariables.ps1
@@ -1,12 +1,11 @@
 $isPreview = "$env:PreviewBuild" -eq [bool]::TrueString
 
-$gitVersionOutput = & "$PSScriptRoot\..\lib\GitVersion\GitVersion.exe"
+$gitVersionOutput = & "$PSScriptRoot\GitVersion\GitVersion.exe"
 $buildVersion = ($gitVersionOutput | ConvertFrom-Json).MajorMinorPatch
 $buildBuild = ($gitVersionOutput | ConvertFrom-Json).BuildMetaData
+$buildLabel = ($gitVersionOutput | ConvertFrom-Json).PreReleaseLabel
 
-$buildLabel = "prerelease"
 $buildConfiguration = "Debug"
-
 $buildVersionSemver = $buildVersion + "-" + $buildLabel + $buildBuild
 
 if (-Not $isPreview)
@@ -16,9 +15,6 @@ if (-Not $isPreview)
 }
 
 $buildVersionComplete = $buildVersionSemver + "+" + (Get-Date).ToString("yyyyMMdd") + "." + $env:BUILD_BUILDID
-
-Write-Host "buildVersion:         " $buildVersion
-Write-Host "buildVersionSemver:   " $buildVersionSemver
 
 Write-Host "##vso[build.updatebuildnumber]" $buildVersionComplete
 Write-Host "##vso[task.setvariable variable=buildVersionSemver]" $buildVersionSemver
