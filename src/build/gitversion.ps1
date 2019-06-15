66b10847f779e03cb1c1c9bfa7263929866c9465
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2019-06-15 22:53:50 +0200

    Added build configuration to versioning file, and reversed IF-logic

--- src/build/gitversion.ps1
+++ src/build/gitversion.ps1
@@ -1,17 +1,21 @@
 $isPreview = "$env:PreviewBuild" -eq [bool]::TrueString
 
 $gitVersionOutput = & "$PSScriptRoot\..\lib\GitVersion\GitVersion.exe"
-
 $buildVersion = ($gitVersionOutput | ConvertFrom-Json).MajorMinorPatch
 $buildBuild = ($gitVersionOutput | ConvertFrom-Json).BuildMetaData
-$buildLabel = "prerelease"
 
-if ($isPreview)
+$buildLabel = "prerelease"
+$buildConfiguration = "Debug"
+
+$buildVersionSemver = $buildVersion + "-" + $buildLabel + $buildBuild
+
+if (-Not $isPreview)
 {
-    $buildVersionSemver = $buildVersion + "-" + $buildLabel + $buildBuild
+    $buildVersionSemver = $buildVersion
+	$buildConfiguration = "Release"
 }
 
-$buildVersionComplete = $buildVersionSemver + "+" + $env:BUILD_BUILDNUMBER
+$buildVersionComplete = $buildVersionSemver + "+" + (Get-Date).ToString("yyyyMMdd") + "." + $env:BUILD_BUILDID
 
 Write-Host "buildVersion:         " $buildVersion
 Write-Host "buildVersionSemver:   " $buildVersionSemver
@@ -19,3 +23,4 @@ Write-Host "buildVersionSemver:   " $buildVersionSemver
 Write-Host "##vso[build.updatebuildnumber]" $buildVersionComplete
 Write-Host "##vso[task.setvariable variable=buildVersionSemver]" $buildVersionSemver
 Write-Host "##vso[task.setvariable variable=buildVersion]" $buildVersion"."$buildBuild
+Write-Host "##vso[task.setvariable variable=buildConfiguration]" $buildConfiguration
