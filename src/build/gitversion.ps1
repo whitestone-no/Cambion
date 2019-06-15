8be6a189ce7937813e52a901110907a254b899fc
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2019-06-15 13:29:08 +0200

    Added script to be used in build

---
+++ src/build/gitversion.ps1
@@ -0,0 +1,16 @@
+$isPreview = "$env:PreviewBuild" -eq [bool]::TrueString
+
+$gitVersionOutput = & "..\lib\GitVersion\GitVersion.exe"
+
+$buildVersion = ($gitVersionOutput | ConvertFrom-Json).MajorMinorPatch
+$buildBuild = ($gitVersionOutput | ConvertFrom-Json).BuildMetaData
+$buildLabel = ($gitVersionOutput | ConvertFrom-Json).PreReleaseLabel
+
+if ($isPreview) {
+    $buildVersion = $buildVersion + "-" + $buildLabel + $buildBuild
+}
+
+$buildVersionComplete = $buildVersion + "+" + $(Date:yyyyMMdd) + $(Rev:.r)
+
+Write-Host "##vso[build.updatebuildnumber]" + $buildVersionComplete
+Write-Host "##vso[task.setvariable variable=buildVersion]" + $buildVersion
