3b27593ec1c2ad15a64dfd997c657046b80d3d80
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2019-07-09 13:40:42 +0200

    Added initial test for Cambion proper

---
+++ src/Cambion.Test/CambionTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using Whitestone.Cambion.Configurations;
+using Whitestone.Cambion.Interfaces;
+using Whitestone.Cambion.Serializer.JsonNet;
+using Whitestone.Cambion.Transport.Loopback;
+
+namespace Whitestone.Cambion.Test
+{
+    [Order(3)]
+    class CambionTests
+    {
+        private ICambion _cambion;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cambion = new CambionConfiguration()
+                .Serializer.UseJsonNet()
+                .Transport.UseLoopback()
+                .Create();
+        }
+
+
+        [Test]
+        public void Register_NullValue_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => { _cambion.Register(null); });
+        }
+    }
+}
