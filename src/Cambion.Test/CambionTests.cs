529087b45bd29fccfeee5dd76e8a5abbb586f38b
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2019-07-20 20:34:54 +0200

    Added event+synchronized tests

--- src/Cambion.Test/CambionTests.cs
+++ src/Cambion.Test/CambionTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Threading;
 using Whitestone.Cambion.Configurations;
 using Whitestone.Cambion.Interfaces;
 using Whitestone.Cambion.Serializer.JsonNet;
@@ -37,20 +38,130 @@ namespace Whitestone.Cambion.Test
 
             Assert.Throws<ArgumentException>(() => _cambion.Register(obj));
         }
+
+        [Test]
+        public void CallEventHandler_DefaultObjects_InterfaceSubscription()
+        {
+            string value = Guid.NewGuid().ToString();
+
+            EventHandler obj = new EventHandler();
+            _cambion.Register(obj);
+
+            _cambion.PublishEvent(new TestEvent(value));
+
+            TestEvent @event = obj.GetEvent();
+
+            Assert.AreEqual(@event.Value, value);
+        }
+
+        [Test]
+        public void CallEventHandler_DefaultObjects_DirectSubscription()
+        {
+            ManualResetEvent mre = new ManualResetEvent(false);
+            string value = Guid.NewGuid().ToString();
+            string response = null;
+
+            _cambion.AddEventHandler<TestEvent>(e =>
+            {
+                response = e.Value;
+                mre.Set();
+            });
+
+            _cambion.PublishEvent(new TestEvent(value));
+
+            mre.WaitOne(new TimeSpan(0, 0, 5));
+
+            Assert.AreEqual(response, value);
+        }
+
+        [Test]
+        public void CallSynchronizedHandler_DefaultObjects_InterfaceSubscription()
+        {
+            string value = Guid.NewGuid().ToString();
+
+            SynchronizedHandler obj = new SynchronizedHandler(value);
+            _cambion.Register(obj);
+
+            TestResponse response = _cambion.CallSynchronizedHandler<TestRequest, TestResponse>(new TestRequest());
+
+            Assert.AreEqual(response.Value, value);
+        }
+
+        [Test]
+        public void CallSynchronizedHandler_DefaultObjects_DirectSubscription()
+        {
+            string value = Guid.NewGuid().ToString();
+
+            SynchronizedHandler obj = new SynchronizedHandler(value);
+
+            _cambion.AddSynchronizedHandler<TestRequest, TestResponse>(req => new TestResponse(value));
+
+            TestResponse response = _cambion.CallSynchronizedHandler<TestRequest, TestResponse>(new TestRequest());
+
+            Assert.AreEqual(response.Value, value);
+        }
     }
 
-    class TestEvent { }
+    class TestEvent
+    {
+        public string Value { get; }
+
+        public TestEvent(string value)
+        {
+            Value = value;
+        }
+    }
 
     class TestRequest { }
 
-    class TestResponse { }
+    class TestResponse
+    {
+        public string Value { get; }
+
+        public TestResponse(string value)
+        {
+            Value = value;
+        }
+    }
 
 
     class TwoOfSameObjectTest : ISynchronizedHandler<TestRequest, TestResponse>
     {
         public TestResponse HandleSynchronized(TestRequest input)
         {
-            return new TestResponse();
+            return new TestResponse(null);
+        }
+    }
+    class SynchronizedHandler : ISynchronizedHandler<TestRequest, TestResponse>
+    {
+        private readonly string _value;
+
+        public SynchronizedHandler(string value)
+        {
+            _value = value;
+        }
+
+        public TestResponse HandleSynchronized(TestRequest input)
+        {
+            return new TestResponse(_value);
+        }
+    }
+
+    class EventHandler : IEventHandler<TestEvent>
+    {
+        private readonly ManualResetEvent _mre = new ManualResetEvent(false);
+        private TestEvent _eventValue;
+
+        public void HandleEvent(TestEvent input)
+        {
+            _eventValue = input;
+            _mre.Set();
+        }
+
+        public TestEvent GetEvent()
+        {
+            _mre.WaitOne(new TimeSpan(0, 0, 5));
+            return _eventValue;
         }
     }
 }
