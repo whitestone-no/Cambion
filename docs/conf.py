94c0957ec9cac94eac0c1d17acb198752fb585c7
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2020-04-22 18:07:10 +0200

    3: more sphinx experimentation

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+import time
+
+source_suffix = ['.rst']
+
+master_doc = 'index'
+
+project = 'Cambion'
+copyright = time.localtime().tm_year + 'Whitestone'
+author = 'Whitestone'
+
+highlight_language = 'csharp'
