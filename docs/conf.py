82755fd0d0a8c8218c0b192a2a57eb5395629691
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2020-04-24 22:38:59 +0200

    3: Removed html_logo again as html_theme_options doesn't work (yet)

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,10 @@ master_doc = 'index'
 highlight_language = 'csharp'
 
 html_favicon = 'favicon.ico'
-html_logo = 'images/logoTiny.png'
+# html_logo = 'images/logoTiny.png'
+html_scaled_image_link = False
 html_theme_options = {
-    'logo_only': True,
+#   'logo_only': True,
     'collapse_navigation': False,
     'prev_next_buttons_location': 'None'
 }
