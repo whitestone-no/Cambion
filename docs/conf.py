f968ddef055bc71b015941f19c986fa604d90dfa
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2020-04-24 22:35:22 +0200

    3: More html_logo test

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@ highlight_language = 'csharp'
 html_favicon = 'favicon.ico'
 html_logo = 'images/logoTiny.png'
 html_theme_options = {
+    'logo_only': True,
     'collapse_navigation': False,
     'prev_next_buttons_location': 'None'
 }
