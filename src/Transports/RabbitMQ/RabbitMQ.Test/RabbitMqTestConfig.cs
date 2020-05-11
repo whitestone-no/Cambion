43c7094beec21fa0297fef66397bb5cd431b6eab
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2020-05-11 20:23:42 +0200

    4: Added basic test framework for RabbitMQ Transport.

---
+++ src/Transports/RabbitMQ/RabbitMQ.Test/RabbitMqTestConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace RabbitMQ.Test
+{
+    internal class RabbitMqTestConfig
+    {
+        public string ConnectionString { get; set; }
+        public string Hostname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string VirtualHost { get; set; }
+    }
+}
---
+++ src/Transports/RabbitMQ/RabbitMQ.Test/RabbittMqTransportTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Whitestone.Cambion.Serializer.JsonNet;
+using Whitestone.Cambion.Transport.RabbitMQ;
+
+namespace RabbitMQ.Test
+{
+    class RabbitMqTransportTests
+    {
+        private RabbitMqTransport _transport;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            IConfigurationRoot configBuilder = new ConfigurationBuilder()
+                .AddUserSecrets("f7583692-a0ea-4e75-a825-9e61b8996832")
+                .AddEnvironmentVariables()
+                .Build();
+
+            RabbitMqTestConfig config = configBuilder.GetSection("RabbitMQ").Get<RabbitMqTestConfig>();
+
+            _transport = new RabbitMqTransport(config.ConnectionString)
+            {
+                Serializer = new JsonNetSerializer()
+            };
+            _transport.Start();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _transport.Stop();
+        }
+
+        [Test]
+        public void Publish_NullValue_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => { _transport.Publish(null); });
+        }
+	}
+}
