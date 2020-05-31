70967c90cd7eb8e0a5e8d8aa59de3efd089e60f7
Gina Therese Hvidsten <gthvidsten@gmail.com>
2020-05-31 21:45:17 +0200

    5: Made class "internal" as it should only be available for this project

--- src/Transports/AzureServiceBus/AzureServiceBus/AsyncAwaitHelper.cs
+++ src/Transports/AzureServiceBus/AzureServiceBus/AsyncAwaitHelper.cs
@@ -5,27 +5,15 @@ using System.Threading.Tasks;
 
 namespace Whitestone.Cambion.Transport.AzureSericeBus
 {
-    public static class AsyncAwaitHelper
+    internal static class AsyncAwaitHelper
     {
         private static readonly TaskFactory MyTaskFactory = new TaskFactory(CancellationToken.None,
             TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
 
-        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        internal static void RunSync(Func<Task> func)
         {
-            var cultureUi = CultureInfo.CurrentUICulture;
-            var culture = CultureInfo.CurrentCulture;
-            return MyTaskFactory.StartNew(() =>
-            {
-                Thread.CurrentThread.CurrentCulture = culture;
-                Thread.CurrentThread.CurrentUICulture = cultureUi;
-                return func();
-            }).Unwrap().GetAwaiter().GetResult();
-        }
-
-        public static void RunSync(Func<Task> func)
-        {
-            var cultureUi = CultureInfo.CurrentUICulture;
-            var culture = CultureInfo.CurrentCulture;
+            CultureInfo cultureUi = CultureInfo.CurrentUICulture;
+            CultureInfo culture = CultureInfo.CurrentCulture;
             MyTaskFactory.StartNew(() =>
             {
                 Thread.CurrentThread.CurrentCulture = culture;
