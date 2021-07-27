8bc41d5e90707c0470909d9d6540cebd95136dd3
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2021-07-27 21:09:09 +0200

    #9: Added MessagePack serializer

---
+++ src/Serializers/MessagePack/MessagePack/MessagePackSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Whitestone.Cambion.Interfaces;
+using Whitestone.Cambion.Types;
+
+using MessagePack_Serializer = MessagePack.MessagePackSerializer;
+
+namespace Whitestone.Cambion.Serializer.MessagePack
+{
+    public class MessagePackSerializer : ISerializer
+    {
+        public async Task<byte[]> Serialize(MessageWrapper message)
+        {
+            MemoryStream ms = new MemoryStream();
+            await MessagePack_Serializer.Typeless.SerializeAsync(ms, message);
+
+            return ms.ToArray();
+        }
+
+        public async Task<MessageWrapper> Deserialize(byte[] serialized)
+        {
+            MemoryStream ms = new MemoryStream(serialized);
+            MessageWrapper message = await MessagePack_Serializer.Typeless.DeserializeAsync(ms) as MessageWrapper;
+
+            return message;
+        }
+    }
+}
---
+++ src/Serializers/MessagePack/MessagePack/MessagePackSerializerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Whitestone.Cambion.Interfaces;
+
+namespace Whitestone.Cambion.Serializer.MessagePack
+{
+    public static class MessagePackSerializerExtensions
+    {
+        public static ICambionBuilder UseMessagePackSerializer(this ICambionBuilder builder)
+        {
+            builder.Services.Replace(new ServiceDescriptor(typeof(ISerializer), typeof(MessagePackSerializer), ServiceLifetime.Singleton));
+
+            return builder;
+        }
+    }
+}
