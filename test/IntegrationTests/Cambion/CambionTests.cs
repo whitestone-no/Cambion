7ee53a0744e55d457f45a1192da70d9d8eb2a6b6
Gina Therese Hvidsten <gthvidsten@whitestone.no>
2021-08-03 22:07:28 +0200

    Removed some duplicated files in solution. Added Cambion integration test skeleton

---
+++ test/IntegrationTests/Cambion/CambionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Whitestone.Cambion.Serializer.JsonNet;
+using Whitestone.Cambion.Transport.Loopback;
+
+namespace Whitestone.Cambion.IntegrationTests.Cambion
+{
+    public class CambionTests
+    {
+        private readonly Whitestone.Cambion.Cambion _cambion;
+
+        public CambionTests()
+        {
+            Mock<ILogger<Whitestone.Cambion.Cambion>> logger = new Mock<ILogger<Whitestone.Cambion.Cambion>>();
+
+            _cambion = new Whitestone.Cambion.Cambion(new LoopbackTransport(), new JsonNetSerializer(), logger.Object);
+        }
+    }
+}
